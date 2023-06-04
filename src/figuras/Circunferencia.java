2c739a2e724d77bec0092a46dcb2f6ec9ba60a44
kellyngton <aitordelapaterna2@gmail.com>
2023-06-04 13:14:05 +0100

    Refactored and implemented some new comments.

--- src/figuras/Circunferencia.java
+++ src/figuras/Circunferencia.java
@@ -1,14 +1,18 @@
 package figuras;
 
+
 public class Circunferencia {
+    // En esta clase declaramos la circunferencia.
     private final double radio;
     private String color;
 
     public Circunferencia(double radio) {
+        //Constructor con el que podemos crear instancias de la clase.
         this.radio = radio;
     }
 
     public void imprimir() {
+        // Este método aunque actualmente no tiene uso sirve para ver las propiedades: Color, Diametro y área de la Circunferencia.
         color = "rojo";
         System.out.println("Di�metro: " + 2 * radio);
         System.out.println("Color: " + color);
@@ -17,6 +21,12 @@ public class Circunferencia {
     }
 
     public boolean esIgual(boolean considerarDecimales, Circunferencia otro) {
+        /**
+         * Compara si dos circunferencias son iguales en términos de radio, considerando la opción de decimales.
+         * @param considerarDecimales indica si se deben considerar los decimales al realizar la comparación
+         * @param otro la otra circunferencia a comparar
+         * @return true si las circunferencias son iguales, false en caso contrario
+         */
         double radio1 = this.radio;
         double radio2 = otro.getRadio();
         if (considerarDecimales) {
@@ -27,11 +37,10 @@ public class Circunferencia {
     }
 
     public double getRadio() {
+        // El método simplemente devuelve el valor del atributo radio.
         return radio;
     }
 
-    public double setRad() {
-        return radio;
-    }
+    //Eliminamos el "Setter" al ser círculo final y no necesitarlo.
 }
 
--- src/figuras/Test.java
+++ src/figuras/Test.java
@@ -2,6 +2,16 @@ package figuras;
 
 public class Test {
     public static void main(String[] args) {
+        /**
+         * El método c2.esIgual se utiliza para comparar si dos circunferencias son iguales.
+         * En este código, se crean tres instancias de la clase Circunferencia: c1, c2 y c3,
+         * con diferentes valores de radio. Luego, utilizamos el método esIgual en c2 para
+         * compararlo con c3 de dos formas:
+         *   - Sin considerar decimales: el método devuelve verdadero si los radios son exactamente iguales.
+         *   - Considerando decimales: el método devuelve verdadero si la diferencia entre los radios es menor a 1.
+         * Si las comparaciones son verdaderas, se imprime un mensaje indicando que las circunferencias son iguales
+         * en el if correspondiente.
+         **/
         Circunferencia c1 = new Circunferencia(5.5);
         Circunferencia c2 = new Circunferencia(10.1);
         Circunferencia c3 = new Circunferencia(10.9);
